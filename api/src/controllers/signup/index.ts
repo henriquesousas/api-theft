9b84dc66b5657a455fe90fe9105fdc48b9ffc535
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-07 10:23:22 -0300

    refactor: added index file to import all protocols from signup controller

---
+++ api/src/controllers/signup/index.ts
@@ -0,0 +1,5 @@
+
+export * from '../../helpers/http/http-request'
+export * from '../../helpers/http/http-response'
+export * from '../../domain/validators/validation'
+export * from '../controller'
