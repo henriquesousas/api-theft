d7c6b09167b69fa33462eb2c6aceecdab32b7425
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-12 19:18:10 -0300

    feat: debug file configured

--- api/src/main/server.ts
+++ api/src/main/server.ts
@@ -1,4 +1,4 @@
-import { MongoHelper } from '../infra/repository/mongo/helper/mongo-helper'
+import { MongoHelper } from '../infra/repository/helper/mongo-helper'
 import env from './config/env'
 
 MongoHelper.connect('mongodb://localhost:27017/theft')
