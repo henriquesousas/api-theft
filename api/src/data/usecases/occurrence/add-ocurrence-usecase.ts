ad63645ce873e0381b304e9fe2ba2e78bcdc2b12
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-20 16:23:36 -0300

    refactor: add occurrence use case refactor

--- api/src/data/usecases/occurrence/add-ocurrence-usecase.ts
+++ api/src/data/usecases/occurrence/add-ocurrence-usecase.ts
@@ -1,11 +1,17 @@
 import { OccurrenceDto } from '../../../domain/dto/occurrence-dto'
 import { AddOccurrence } from '../../../domain/usecases/occurrence/add-occurrence'
+import { UnauthorizedError } from '../../../helpers/erros/unauthorized-error'
+import { LoadAccountByIdRepository } from '../../protocols/repository/account/load-account-by-id-repository'
 import { AddOccurrenceRepositoy } from '../../protocols/repository/ocurrence/add-occurrence-repository'
 
 export class AddOccurrenceUsecase implements AddOccurrence {
-  constructor (private readonly addOccurrenceRepository: AddOccurrenceRepositoy) {}
+  constructor(
+    private readonly addOccurrenceRepository: AddOccurrenceRepositoy,
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository) { }
 
   async add(dto: OccurrenceDto): Promise<void> {
+    const account = await this.loadAccountByIdRepository.loadById(dto.userId)
+    if (!account) throw new UnauthorizedError()
     await this.addOccurrenceRepository.add(dto)
   }
 }
--- api/test/data/usecases/add-occurrence-usecase.spec.ts
+++ api/test/data/usecases/add-occurrence-usecase.spec.ts
@@ -1,12 +1,32 @@
+import { LoadAccountByIdRepository } from '../../../src/data/protocols/repository/account/load-account-by-id-repository'
+import { AddOccurrenceRepositoy } from '../../../src/data/protocols/repository/ocurrence/add-occurrence-repository'
 import { AddOccurrenceUsecase } from '../../../src/data/usecases/occurrence/add-ocurrence-usecase'
+import { UnauthorizedError } from '../../../src/helpers/erros/unauthorized-error'
 import { mockOccurrenceDto } from '../../domain/dto/mock-occurrence-dto'
+import { mockLoadAccountByIdRepository } from '../../infra/repository/mocks'
 import { mockAddOccurrenceRepository } from '../../infra/repository/mocks/mock-add-occurrence-repository'
+
+type SutTypes = {
+  sut: AddOccurrenceUsecase
+  addOccurrenceRepositoyStub: AddOccurrenceRepositoy
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+}
+
+const mockSut = (): SutTypes => {
+  const addOccurrenceRepositoyStub = mockAddOccurrenceRepository()
+  const loadAccountByIdRepositoryStub = mockLoadAccountByIdRepository()
+  const sut = new AddOccurrenceUsecase(addOccurrenceRepositoyStub, loadAccountByIdRepositoryStub)
+  return {
+    sut,
+    addOccurrenceRepositoyStub,
+    loadAccountByIdRepositoryStub
+  }
+}
 
 describe('AddOccurrenceUseCase', () => {
   describe('AddOccurrenceRepositoy', () => {
     test('deve chamar AddOccurrenceRepositoy com os valores corretos', async () => {
-      const addOccurrenceRepositoyStub = mockAddOccurrenceRepository()
-      const sut = new AddOccurrenceUsecase(addOccurrenceRepositoyStub)
+      const { sut, addOccurrenceRepositoyStub } = mockSut()
       const addSpy = jest.spyOn(addOccurrenceRepositoyStub, 'add')
       const dto = mockOccurrenceDto()
       await sut.add(dto)
@@ -14,10 +34,27 @@ describe('AddOccurrenceUseCase', () => {
     })
 
     test('deve lançar uma exception se o AddOccurrenceRepositoy throws', async () => {
-      const addOccurrenceRepositoyStub = mockAddOccurrenceRepository()
-      const sut = new AddOccurrenceUsecase(addOccurrenceRepositoyStub)
+      const { sut, addOccurrenceRepositoyStub } = mockSut()
       jest.spyOn(addOccurrenceRepositoyStub, 'add').mockImplementationOnce(async () => await Promise.reject(new Error()))
       const promise = sut.add(mockOccurrenceDto())
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('LoadAccountByIdRepository', () => {
+    test('deve chamar LoadAccountByIdRepository (add) com o id correto', async () => {
+      const { sut, loadAccountByIdRepositoryStub } = mockSut()
+      const loadSpy = jest.spyOn(loadAccountByIdRepositoryStub, 'loadById')
+      const dto = mockOccurrenceDto()
+      await sut.add(dto)
+      expect(loadSpy).toHaveBeenCalledWith(dto.userId)
+    })
+
+    test('deve lançar exceção UnauthorizedError se LoadAccountByIdRepository (loadById) não encontrar uma conta', async () => {
+      const { sut, loadAccountByIdRepositoryStub } = mockSut()
+      const dto = mockOccurrenceDto()
+      jest.spyOn(loadAccountByIdRepositoryStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+      const promise = sut.add(dto)
       await expect(promise).rejects.toThrow()
     })
   })
