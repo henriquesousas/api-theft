a860deeb3fa4274f8baf7776415c86554d95a055
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-24 17:32:26 -0300

    feat: mock account

---
+++ api/test/data/models/mock-account.ts
@@ -0,0 +1,10 @@
+import { Account } from '../../../src/domain/models/account'
+
+export const mockAccountModel = (): Account => {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password'
+  }
+}
