bc43e295dc9cc3dfb0a256ba0861072f107ac7de
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-26 17:54:11 -0300

    refactor: changed request variable fill

--- src/main/adapters/express-router-adapter.ts
+++ src/main/adapters/express-router-adapter.ts
@@ -1,13 +1,13 @@
-import { Controller, HttpRequest } from '../../controllers/import-protocols'
+import { Controller } from '../../controllers/import-protocols'
 import { Request, Response } from 'express'
 
 export const expressRouterAdapter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body,
-      params: req.params
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {})
     }
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode === 200) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
