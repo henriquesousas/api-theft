143b7843f6df6834bdf5fa91e78e8e0153c3f200
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-26 14:22:12 -0300

    test: created a jwt test

---
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from '../../../src/infra/criptography/jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return await new Promise(resolve => resolve('any_token'))
+  }
+}))
+
+describe('JWT', () => {
+  test('Deve chamar o JWT com os valores correto', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Deve retornar um accessToken se o JWT sign com sucesso', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
+  })
+
+  test('Deve throws se o JWT sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    expect(promise).rejects.toThrow()
+  })
+})
