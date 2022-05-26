3ad90e63ce1bc8701b6f975be0a90e4a2a160d15
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-26 10:51:34 -0300

    feat: added a load occurrence by id router

--- src/main/routers/occurrences-router.ts
+++ src/main/routers/occurrences-router.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
+import { makeLoadOccurrenceControllerFactory } from 'main/factories/controllers/loado-occurrence-controller.factory'
 import { expressRouterAdapter } from '../adapters/express-router-adapter'
-import { makeOccurrenceControllerFactory } from '../factories/controllers/occurrence-controller.factory'
+import { makeCreateOccurrenceControllerFactory } from '../factories/controllers/create-occurrence-controller.factory'
 
 export default (router: Router): void => {
-  router.use('/occurrence', expressRouterAdapter(makeOccurrenceControllerFactory()))
+  router.use('/occurrence', expressRouterAdapter(makeCreateOccurrenceControllerFactory()))
+  router.use('/occurrence/{id}', expressRouterAdapter(makeLoadOccurrenceControllerFactory()))
 }
