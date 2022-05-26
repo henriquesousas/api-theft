f2d69499ee9d03301a790c5414b7567e3b8a8f0d
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-26 10:40:14 -0300

    refactor: fix  to implements LoadAccountByIdRepository

--- src/infra/repository/occurrence-mongo-repository.ts
+++ src/infra/repository/occurrence-mongo-repository.ts
@@ -1,10 +1,10 @@
 import { AddOccurrenceRepositoy } from '../../data/protocols/repository/ocurrence/add-occurrence-repository'
 import { OccurrenceDto } from '../../domain/dto/occurrence-dto'
-import { LoadOccurrenceById } from '../../domain/usecases/occurrence/load-occurrence-by-id'
 import { MongoHelper } from './helper/mongo-helper'
 import { ObjectID } from 'mongodb'
+import { LoadOccurrenceByIdRepository } from 'data/protocols/repository/ocurrence/load-occurrence-by-id-repository'
 
-export class OccurrenceMongoRepository implements AddOccurrenceRepositoy, LoadOccurrenceById {
+export class OccurrenceMongoRepository implements AddOccurrenceRepositoy, LoadOccurrenceByIdRepository {
   async add(dto: OccurrenceDto): Promise<void> {
     const occurrenceCollection = await MongoHelper.getCollection('occurrences')
     await occurrenceCollection.insertOne(dto)
