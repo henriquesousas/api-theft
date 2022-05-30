6ea053f5cb968936b9ee62459de6e251b4a57012
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-30 11:24:30 -0300

    test: added a jwt test

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -8,23 +8,29 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('JWT', () => {
-  test('Deve chamar o JWT com os valores correto', async () => {
-    const sut = new JwtAdapter('secret')
-    const signSpy = jest.spyOn(jwt, 'sign')
-    sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  describe('sign', () => {
+    test('Deve chamar o JWT com os valores correto', async () => {
+      const sut = new JwtAdapter('secret')
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+
+    test('Deve retornar um accessToken se o JWT sign com sucesso', async () => {
+      const sut = new JwtAdapter('secret')
+      const accessToken = await sut.encrypt('any_id')
+      expect(accessToken).toBe('any_token')
+    })
+
+    test('Deve throws se o JWT sign throws', async () => {
+      const sut = new JwtAdapter('secret')
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 
-  test('Deve retornar um accessToken se o JWT sign com sucesso', async () => {
-    const sut = new JwtAdapter('secret')
-    const accessToken = await sut.encrypt('any_id')
-    expect(accessToken).toBe('any_token')
-  })
+  describe('Decrypter', () => {
 
-  test('Deve throws se o JWT sign throws', async () => {
-    const sut = new JwtAdapter('secret')
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
-    const promise = sut.encrypt('any_id')
-    expect(promise).rejects.toThrow()
   })
 })
--- tests/middleware/login-middleware.spec.ts
+++ tests/middleware/login-middleware.spec.ts
@@ -1,11 +1,43 @@
 import { LoginMiddleware } from '../../src/middlewares/login-middleware'
 import { forbidden } from '../../src/helpers/http/http'
 import { AccessDeniedError } from '../../src/helpers/erros/access-denied-error'
+import { LoadAccountByToken } from '../domain/usecases/middleware/load-account-by-token'
+import { HttpRequest } from '../helpers/http/http-request'
 
 describe('LoginMiddleware', () => {
   test('Deve retornar 403 se não informado o x-access-token no header ', async () => {
-    const sut = new LoginMiddleware()
+    class LoadAccountByTokenUseCaseStub implements LoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
+        return await new Promise(resolve => resolve({
+          id: 'any_id'
+        }))
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByTokenUseCaseStub()
+    const sut = new LoginMiddleware(loadAccountByTokenStub, 'admin')
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Deve chamar o LoadAccountByToken com o valor correto', async () => {
+    class LoadAccountByTokenUseCaseStub implements LoadAccountByToken {
+      async load(accessToken: string, role?: string): Promise<LoadAccountByToken.Result> {
+        return await new Promise(resolve => resolve({
+          id: 'any_id'
+        }))
+      }
+    }
+
+    const loadAccountByTokenStub = new LoadAccountByTokenUseCaseStub()
+    const sut = new LoginMiddleware(loadAccountByTokenStub, 'admin')
+    const httpRequest: HttpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    await sut.handle(httpRequest)
+    expect(loadSpy).toHaveBeenCalledWith('any_token', 'admin')
   })
 })
