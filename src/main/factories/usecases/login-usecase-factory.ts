1732d2e0d993d7a9a0e9ac64659ad21bb5ea0d1d
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-30 11:23:36 -0300

    feat: added middleware

--- src/main/factories/usecases/login-usecase-factory.ts
+++ src/main/factories/usecases/login-usecase-factory.ts
@@ -3,11 +3,12 @@ import { LoginUseCase } from '../../../data/usecases/account/login-usecase'
 import { Authentication } from '../../../domain/usecases/account/authentication'
 import { BCrypter } from '../../../infra/criptography/bcrypter'
 import { AccountMongoRepositoy } from '../../../infra/repository/account-mongo-repository'
+import env from '../../../main/config/env'
 
 export const makeLoginUseCaseFactory = (): Authentication => {
   const salt = 12
   const hasher = new BCrypter(salt)
-  const encrypter = new JwtAdapter('secret')
+  const encrypter = new JwtAdapter(env.jwtSecret)
   const loadAccountByEmailRepository = new AccountMongoRepositoy()
   const updateAccessTokenRepository = new AccountMongoRepositoy()
   return new LoginUseCase(loadAccountByEmailRepository, updateAccessTokenRepository, hasher, encrypter)
--- src/middlewares/login-middleware.ts
+++ src/middlewares/login-middleware.ts
@@ -2,10 +2,24 @@ import { HttpRequest, HttpResponse } from '../controllers/import-protocols'
 import { Middleware } from './middleware'
 import { forbidden } from '../helpers/http/http'
 import { AccessDeniedError } from '../helpers/erros/access-denied-error'
+import { ErrorFactory } from '../helpers/erros/factory/error-factory'
+import { LoadAccountByToken } from 'domain/usecases/middleware/load-account-by-token'
 
 export class LoginMiddleware implements Middleware {
+  constructor(
+    private readonly loadAccountByToken: LoadAccountByToken,
+    private readonly role?: string
+  ) { }
+
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const error = forbidden(new AccessDeniedError())
-    return await new Promise(resolve => resolve(error))
+    try {
+      const accessToken = request.headers?.['x-access-token']
+      if (accessToken) {
+        await this.loadAccountByToken.load(accessToken, this.role)
+      }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return new ErrorFactory().get(error)
+    }
   }
 }
