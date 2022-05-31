67d7c52d1bd562ac024069982699843b3f3d7b41
henriquesousas <paulohenriquess2014@gmail.com>
2022-05-31 15:03:16 -0300

    feat: added path alias - module imports

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,5 @@
-import { MongoHelper } from '../infra/repository/helper/mongo-helper'
+import 'module-alias/register'
+import { MongoHelper } from '@/infra/repository/helper/mongo-helper'
 import env from './config/env'
 
 MongoHelper.connect('mongodb://localhost:27017/theft')
