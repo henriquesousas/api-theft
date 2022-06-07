d4a092f64d0d30a0863494982af4a70587c43970
henriquesousas <paulohenriquess2014@gmail.com>
2022-06-07 14:08:01 -0300

    feat: added swagger basic configuration

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import Express from 'express'
 import setupMiddlewares from './middleware'
 import setupRouter from './router'
+import setupSwagger from './config-swagger'
 
 const app = Express()
+setupSwagger(app)
 setupMiddlewares(app)
 setupRouter(app)
 export default app
